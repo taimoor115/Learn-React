cfc4d5d11b06317c1b52c57f55cf43d533867aae
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-09-11 20:06:59 +0500

    Context API

--- 6.context-api/src/components/Cart.jsx
+++ 6.context-api/src/components/Cart.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
-import { CartContext } from "../context/Cart";
+import { useCart } from "../context/Cart";
 
 const Cart = () => {
+  const cart = useCart();
+  const total = cart.items.reduce((a, b) => a + b.price, 0);
   return (
     <div className="item-cart">
       <h1>Cart</h1>
-      <h5>Total Bill: $</h5>
+      {cart &&
+        cart.items.map((item) => (
+          <li>
+            {item.name} -$ {item.price}{" "}
+          </li>
+        ))}
+
+      <h5>Total Bill: ${total}</h5>
     </div>
   );
 };
--- 6.context-api/src/context/Cart.jsx
+++ 6.context-api/src/context/Cart.jsx
@@ -1,13 +1,16 @@
-import { createContext } from "react";
-import React, { useState } from 'react';
+import { createContext, useContext } from "react";
+import React, { useState } from "react";
 
-
+export const useCart = () => {
+    const cart = useContext(CartContext);
+    return cart;
+}
 export const CartContext = createContext(null);
 export const CartProvider = (props) => {
   const [items, setItems] = useState([]);
   return (
-  <CartContext.Provider value={{items, setItems}}>
-    {props.children}
+    <CartContext.Provider value={{ items, setItems }}>
+      {props.children}
     </CartContext.Provider>
   );
 };
