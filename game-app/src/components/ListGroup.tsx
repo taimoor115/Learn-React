829fe85ef82d3d4e10f1aa4914e1304dde370e10
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-17 13:12:55 +0500

    Event Handler

--- game-app/src/components/ListGroup.tsx
+++ game-app/src/components/ListGroup.tsx
@@ -1,6 +1,7 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["Lahore", "Karachi", "Multan", "Islamabad", "Sindh"];
-  items = [];
+  // items = [];
 
   // we can do the same thing in better way in below jsx
   // if (items.length === 0)
@@ -15,9 +16,12 @@ function ListGroup() {
   // const message = items.length === 0 ? <p>No items Found</p> : null;
 
   // With Function
-  const getMessage = () => {
-    return items.length === 0 ? <p>No items Found</p> : null;
-  };
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No items Found</p> : null;
+  // };
+
+  // The function that handle the event called event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   // In runtime react do React.create Element('h1') but at a time it can create one
   // element but below we use separate tags like h1 and ul so getting the errors
   // There are two ways to remove errors
@@ -33,12 +37,14 @@ function ListGroup() {
       {/* {items.length === 0 ? <p>No items found</p> : null} */}
       {/* 1.{getMessage()} */}
       {/* 2.{message} */}
-      {items.length === 0 && <p>No items Found</p>}
+      {/* {items.length === 0 && <p>No items Found</p>} */}
 
       {/* Render list  */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" onClick={handleClick} key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
