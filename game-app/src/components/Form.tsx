5650dffbaec9ff94e7d915b7934049f1ca71d67b
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-26 11:53:19 +0500

    Form value using useState Hook

--- game-app/src/components/Form.tsx
+++ game-app/src/components/Form.tsx
@@ -1,14 +1,20 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const ageRef = useRef<HTMLInputElement>(null);
 
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
   const handleOnSubmit = (event: FormEvent) => {
     event.preventDefault();
 
-    if (nameRef.current !== null) console.log(nameRef.current.value);
-    if (ageRef.current !== null) console.log(ageRef.current.value);
+    console.log(person);
+
+    // if (nameRef.current !== null) console.log(nameRef.current.value);
+    // if (ageRef.current !== null) console.log(ageRef.current.value);
   };
   return (
     <>
@@ -17,13 +23,26 @@ const Form = () => {
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input ref={nameRef} type="text" id="name" className="form-control" />
+          {/* ref={nameRef} */}
+          <input
+            onChange={(event) =>
+              setPerson({ ...person, name: event.target.value })
+            }
+            value={person.name}
+            type="text"
+            id="name"
+            className="form-control"
+          />
           <div className="mb-3">
             <label htmlFor="age" className="form-label">
               Age
             </label>
             <input
-              ref={ageRef}
+              // ref={ageRef}
+              onChange={(event) =>
+                setPerson({ ...person, age: event.target.value })
+              }
+              value={person.age}
               type="number"
               id="age"
               className="form-control"
