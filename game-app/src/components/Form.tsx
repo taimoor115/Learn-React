f453db9512822660cd29b27cf2d261a78b7b4a80
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-26 12:31:29 +0500

    Form-Hook Form validation

--- game-app/src/components/Form.tsx
+++ game-app/src/components/Form.tsx
@@ -1,52 +1,72 @@
 import { FormEvent, useRef, useState } from "react";
-
+import { FieldValues, useForm } from "react-hook-form";
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onHandleSubmit = (data: FieldValues) => console.log(data);
+
   // const nameRef = useRef<HTMLInputElement>(null);
   // const ageRef = useRef<HTMLInputElement>(null);
 
-  const [person, setPerson] = useState({
-    name: "",
-    age: "",
-  });
-  const handleOnSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  // const [person, setPerson] = useState({
+  //   name: "",
+  //   age: "",
+  // });
+  // const handleOnSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
 
-    console.log(person);
+  //   console.log(person);
 
-    // if (nameRef.current !== null) console.log(nameRef.current.value);
-    // if (ageRef.current !== null) console.log(ageRef.current.value);
-  };
+  //   // if (nameRef.current !== null) console.log(nameRef.current.value);
+  //   // if (ageRef.current !== null) console.log(ageRef.current.value);
+  // };
   return (
     <>
-      <form action="" onSubmit={handleOnSubmit}>
+      <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Name
           </label>
           {/* ref={nameRef} */}
           <input
-            onChange={(event) =>
-              setPerson({ ...person, name: event.target.value })
-            }
-            value={person.name}
+            // onChange={(event) =>
+            //   setPerson({ ...person, name: event.target.value })
+            // }
+            // value={person.name}
+            {...register("name", { required: true, minLength: 3 })}
             type="text"
             id="name"
             className="form-control"
           />
+          {errors.name?.type === "required" && (
+            <p className="text-danger">The Field name is required</p>
+          )}
+          {errors.name?.type === "minLength" && (
+            <p className="text-danger">
+              Character should be greater than 3 characters
+            </p>
+          )}
           <div className="mb-3">
             <label htmlFor="age" className="form-label">
               Age
             </label>
             <input
+              {...register("age", { required: true })}
               // ref={ageRef}
-              onChange={(event) =>
-                setPerson({ ...person, age: event.target.value })
-              }
-              value={person.age}
+              // onChange={(event) =>
+              //   setPerson({ ...person, age: event.target.value })
+              // }
+              // value={person.age}
               type="number"
               id="age"
               className="form-control"
             />
+            {errors.name?.type === "required" && (
+              <p className="text-danger">The Field name is required</p>
+            )}
           </div>
         </div>
 
