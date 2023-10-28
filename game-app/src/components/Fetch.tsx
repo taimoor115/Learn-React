93f269591789e0d13ef141f9a8421baf13ae0b56
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-28 23:44:48 +0500

    Fetch data using async await

--- game-app/src/components/Fetch.tsx
+++ game-app/src/components/Fetch.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -9,12 +9,26 @@ const Fetch = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
 
+  // useEffect(() => {
+  //   axios
+  //     .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+  //     .then((res) => setUsers(res.data))
+  //     .catch((err) => setError(err.message));
+  // }, []);
+
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchUser();
+  });
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
