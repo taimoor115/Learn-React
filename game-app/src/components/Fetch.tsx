ffeea82717cc00f8e6efa4b18a9cbec64fab94ea
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-10-28 23:29:49 +0500

    Fetch data through API

--- game-app/src/components/Fetch.tsx
+++ game-app/src/components/Fetch.tsx
@@ -7,18 +7,23 @@ interface User {
 }
 const Fetch = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
