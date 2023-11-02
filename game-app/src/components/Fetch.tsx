dabf2805d1048d7db10e2b8f465d37cc2e37f40b
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-11-02 23:46:25 +0500

    Create, Delete, Update Opertion

--- game-app/src/components/Fetch.tsx
+++ game-app/src/components/Fetch.tsx
@@ -37,7 +37,7 @@ const Fetch = () => {
   //   const fetchUser = async () => {
   //     try {
   //       const res = await axios.get<User[]>(
-  //         "https://jsonplaceholder.typicode.com/xusers"
+  //         "https://jsonplaceholder.typicode.com/users"
   //       );
   //       setUsers(res.data);
   //     } catch (err) {
@@ -46,13 +46,86 @@ const Fetch = () => {
   //   };
   //   fetchUser();
   // });
+
+  // Delete User
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  // Add User Function
+  const addUser = () => {
+    const originalUser = [...users];
+    const newUser = { id: 0, name: "Taimoor Hussain" };
+    setUsers([...users, newUser]);
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      // .then((res) => {setUsers([...users, res.data])})
+      .then(({ data: savedUser }) => {
+        setUsers([...users, savedUser]);
+      })
+      .catch((err) => setError(err.message));
+    setUsers(originalUser);
+  };
+
+  // Update Function
+  const updateUser = (user: User) => {
+    const originalUser = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updateUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUser);
+      });
+  };
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+
+      {/* Add User */}
+      <button className="mb-3 btn btn-primary" onClick={addUser}>
+        {"Add"}
+      </button>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between "
+          >
+            {user.name}
+            <div>
+              {/* Update  */}
+
+              <button
+                className="btn btn-outline-secondary"
+                onClick={() => updateUser(user)}
+              >
+                {"Update"}
+              </button>
+
+              {/* Delete */}
+
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                {"Delete"}
+              </button>
+            </div>
+          </li>
         ))}
       </ul>
     </>
