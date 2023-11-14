c34b915f252c056b2ff691441207385630a2c07a
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-11-15 00:23:53 +0500

    React Router Dom

--- react-router-dom/src/main.jsx
+++ react-router-dom/src/main.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+  NavLink,
+} from "react-router-dom";
 import "./index.css";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      {posts.map((post) => (
+        <NavLink
+          to={`/post/${post.id}`}
+          style={{ display: "block" }}
+          key={post.id}
+        >
+          {post.title}
+        </NavLink>
+      ))}
+    </div>
+  );
 };
 
 const Profile = () => {
@@ -16,10 +41,32 @@ const About = () => {
 };
 
 const Settings = () => {
+  return <h1>Settings</h1>;
+};
+
+const SayUser = () => {
+  const params = useParams();
+  return <div>User Name is {params.userName}</div>;
+};
+
+const PostPage = () => {
+  const params = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+      .then((data) => data.json())
+      .then((data) => setData(data));
+  }, []);
+
+  if(data === null) return <h3>Loading</h3>
   return (
-    <h1>Settings</h1>
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+    </div>
   );
-}
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -27,11 +74,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path='account'>
-          <Route path='profile' element={<Profile />} />
-          <Route path='setting' element={<Settings />} />
+        <Route path="/user/:userName" element={<SayUser />} />
+        <Route path="account">
+          <Route path="profile" element={<Profile />} />
+          <Route path="setting" element={<Settings />} />
         </Route>
-
+        <Route path="/post/:postId" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
