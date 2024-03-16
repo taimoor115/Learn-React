2706e962fd5a52e11c1a4ab7e6c25cf894153ae4
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-03-16 11:38:23 +0500

    Setup Routing

---
+++ redux-toolkit/src/route.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+
+const router = createBrowserRouter([
+  {path: "/", element: <Home />},
+]);
+
+export default router;
