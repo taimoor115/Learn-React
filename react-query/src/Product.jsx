6a8db52676c8123eb09cd3b9d928588ce90d6f9c
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-03 12:35:17 +0500

    fetch specific product based on Id

--- react-query/src/Product.jsx
+++ react-query/src/Product.jsx
@@ -1,7 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router-dom";
+const fetchProducts = async (id) => {
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
+  const data = await response.json();
+  return data;
+};
 
+const useProductsById = (id) => {
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: () => fetchProducts(id),
+    staleTime: 10000,
+  });
+};
 const Product = () => {
-  return <div></div>;
+  const { productId: id } = useParams();
+  console.log(id);
+  const { data: product, isLoading } = useProductsById(id);
+
+  console.log(product);
+  if (isLoading) {
+    return <div>Loading......</div>;
+  }
+  return <div>{product.title}</div>;
 };
 
 export default Product;
