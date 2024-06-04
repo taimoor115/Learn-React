796b623ed8162b07cb0ae8d1daaeb0cdf46c70d9
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-04 23:04:45 +0500

    useMemoHook

--- e-cart/src/App.js
+++ e-cart/src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
+import React, { useMemo, useState } from "react";
+const UseMemoCallback = () => {
+  const [add, setAdd] = useState(0);
+  const [miuns, setMinus] = useState(100);
 
-const App = () => {
+  const multiply = useMemo(() => {
+    console.log("I'm in");
+    return add * 10;
+  }, [add]);
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div>{multiply}</div>
+      <button onClick={() => setAdd(add + 1)}>Add {add}</button>
+      <button onClick={() => setMinus(miuns - 1)}>Minus {miuns}</button>
+    </>
   );
 };
+
+export function App(props) {
+  return (
+    <div className="App">
+      <UseMemoCallback />
+    </div>
+  );
+}
 
 export default App;
--- react-query/src/Product.jsx
+++ react-query/src/Product.jsx
@@ -1,10 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 const fetchProducts = async (id) => {
   const response = await fetch(`https://dummyjson.com/products/${id}`);
   const data = await response.json();
   return data;
+};
+
+const putData = async (id, newData) => {
+  return axios.put(`https://dummyjson.com/products/${id}`, newData);
+};
+
+const usePutData = () => {
+  return useMutation({
+    mutationFn: (id, newData) => putData(id, newData),
+  });
 };
 
 const useProductsById = (id) => {
@@ -19,11 +30,22 @@ const Product = () => {
   console.log(id);
   const { data: product, isLoading } = useProductsById(id);
 
+  const { mutateAsync: putDataOnServer } = usePutData();
   console.log(product);
   if (isLoading) {
     return <div>Loading......</div>;
   }
-  return <div>{product.title}</div>;
+  return (
+    <div>
+      {product.title}
+
+      <div>
+        <button onClick={() => putDataOnServer(id, { title: "Lotions" })}>
+          Create
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
